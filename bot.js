f3c3dfdf15a7cb001c96c5e734c3be4ddb7d7712
bcstore1112 <42453517+bcstore1112@users.noreply.github.com>
2018-08-17 00:51:45 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = "#";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\`** :white_check_mark: جآري آرسال البرودكآست ل  **`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`AG CLAN BOT- Script By : PREDATOR`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : PREDATOR ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` AG STORE.`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+client.login(process.env.BOT_TOKEN);
