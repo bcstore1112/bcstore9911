f3b6d29a6037645ad454c1b559a0fa76df5c7264
bcstore1112 <42453517+bcstore1112@users.noreply.github.com>
2018-09-06 17:48:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-var prefix = "#";
-client.on("message", message => {
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(" تم آرسآل الرآبط لك بالخآص , ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
 
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\`** :white_check_mark: جآري آرسال البرودكآست ل  **`); 
- message.delete(); 
-};     
-});
-
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`AG CLAN BOT- Script By : PREDATOR`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : PREDATOR ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` AG STORE.`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرآبط : سآعة | عدد الآستخدامات : 5 **")
+      message.author.sendEmbed(Embed11)
+client.user.setGame(`3RBE.GQ SERVER`,"https://www.twitch.tv/predator12_")
+    }
+}); 
 
 client.login(process.env.BOT_TOKEN);
